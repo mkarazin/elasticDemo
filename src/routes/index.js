117493df7e7cdb4f8b589e68a1ed56494edc0a66
Matt <matt@medean.com>
2017-05-24 10:00:38 -0500

    Added ElasticSearch - Code during live coding demonstration for tech talk at Fullstack

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,34 @@
 import { Router } from "express"
 import * as ResponseObjects from "./../utils/responseObjects"
+import { ElasticSearchClient as ElasticSearchClient } from './../services/elasticsearch'
 
 const router = new Router()
+
+// Searches for quotes
+router.get(
+  "/quotes",
+  async (req, res) => {
+
+    // Get the query string fom the search term
+    const queryString = req.query.searchTerm
+
+    // Perform the search
+    const results = await ElasticSearchClient.search({
+      body: {
+        "query": {
+          "match" : {
+            "text_entry" : queryString
+          }
+        }
+      }
+    })
+
+    ResponseObjects.Success(res, {
+      results: results.hits.hits
+    })
+
+  }
+)
 
 /**
  * For Testing / checking if the service is running successfully
---
+++ src/services/elasticsearch.js
@@ -0,0 +1,6 @@
+import * as ElasticSearch from 'elasticsearch'
+
+// TODO: move connection string to config or environment variable
+export const ElasticSearchClient = new ElasticSearch.Client({
+  host: 'localhost:9200'
+})
