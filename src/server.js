e12dd9f8867c7d31e7678d0d014a71be9a10089c
Dan Collins <dcollins31@gmail.com>
2019-02-06 19:42:59 -0600

    404 Error

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res) => {
 // Handle 404s
 app.use((req, res, next) => {
   res.status(404).send({
-    message: 'Houston, we\'ve had a problem here',
+    message: 'Error: 404 Not Found',
     urlRequested: `${req.method} ${req.url}`,
   });
 });
