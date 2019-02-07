3be9c5f079560f812a79e067c756cb2306ace12d
DeAndre Boston <dboston3@yahoo.com>
2019-02-06 19:35:48 -0600

    Change 404 error message

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res) => {
 // Handle 404s
 app.use((req, res, next) => {
   res.status(404).send({
-    message: 'Houston, we\'ve had a problem here',
+    message: 'Awe Snap, something is not right!',
     urlRequested: `${req.method} ${req.url}`,
   });
 });
