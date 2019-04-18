6a1f7fe744f5bff3ed56142d655c160261c50736
rmarro <30510953+rmarro@users.noreply.github.com>
2019-04-17 19:20:54 -0500

    changed the 404 message

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res) => {
 // Handle 404s
 app.use((req, res, next) => {
   res.status(404).send({
-    message: 'Houston, we\'ve had a problem here',
+    message: 'Uh oh... nothing found here',
     urlRequested: `${req.method} ${req.url}`,
   });
 });
