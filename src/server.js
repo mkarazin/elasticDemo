3f997e502096100df3430c4143712fd1aab32859
Andrea Minhas <andrea.minhas@gmail.com>
2019-05-08 19:08:20 -0500

    added 4th parameter

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res) => {
 });
 
 // Handle 404s
-app.use((req, res, next) => {
+app.use((req, res, res, next) => {
   res.status(404).send({
     message: "This is not a valid page. Please check your url. ",
     urlRequested: `${req.method} ${req.url}`
